0c9a1d475669ebc26ebe240c8e64405342619b86
Angy Encinas <angy.encinas@ucb.edu.bo>
2022-12-05 21:39:57 -0400

    verifica si la publicacion esta vacia

--- cypress/e2e/sumador.cy.js
+++ cypress/e2e/sumador.cy.js
@@ -5,4 +5,10 @@ describe("Twitter", () => {
     cy.get("#publicar-button").click();
     cy.get("#resultado-div").should("contain", "Hola mundo");
   });
+  it("Deberia mostrarse un mensaje: No se puede ingresar un post sin Texto enla parte superior de la lista de publicaciones. ", () => {
+    cy.visit("/");
+    cy.get("#publicacion").type(" ");
+    cy.get("#publicar-button").click();
+    cy.get("#advertencia-div").should("contain", "No se puede ingresar un post sin Texto");
+  });
 });
--- src/presenter.js
+++ src/presenter.js
@@ -3,12 +3,14 @@ import sumar from "./sumador";
 const publicacion = document.querySelector("#publicacion");
 const form = document.querySelector("#twitter-form");
 const div = document.querySelector("#resultado-div");
-
+const div2 = document.querySelector("#advertencia-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const tweet = publicacion.value;
 
-
   div.innerHTML = "<p>" + tweet + "</p>";
+  if(tweet == " "){
+    div2.innerHTML = "<p>" + "No se puede ingresar un post sin Texto" + "</p>";
+  }
 });
